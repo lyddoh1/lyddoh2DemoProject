d1569d85fa28f07c04652c4c6a8f88e9765a4fc5
alexd <alexd@LAPTOP-34NUR44S.myfiosgateway.com>
2022-05-29 13:07:01 -0400

    COMMIT 2

---
+++ src/demoAssignmentProject/DemoAssignmentProject.java
@@ -0,0 +1,24 @@
+package demoAssignmentProject;
+
+public class DemoAssignmentProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++)
+			if (i%2==0)
+		{
+		System.out.println(i + " This is an even number");
+		}
+		
+		// Reverse
+		for(int i=10;i>=1;i--)
+			if (i%2==0)
+		{
+		System.out.println(i + " This is an even number");
+		}
+		
+
+	}
+
+}
